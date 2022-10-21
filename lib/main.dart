4dea2e09a12d8d191a562bb04e7fe268191f14ec
Mahmoud Alghoraqy <mahmoudalghoraqy@gmail.com>
2022-10-21 21:08:42 +0200

    firebase emulator

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,23 @@
+import 'dart:io';
+
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:xchat/app.dart';
 import 'package:xchat/core/network/local/cach_helper.dart';
 import 'package:xchat/core/utils/constances.dart';
-import 'package:xchat/presentation/screens/Sign%20Up/screen/signup_screen.dart';
 import 'package:xchat/presentation/screens/home/screen/layout/home_screen.dart';
 import 'package:xchat/presentation/screens/login/screen/login_screen.dart';
 import 'package:xchat/presentation/screens/OnBoard/onboard_screen.dart';
 
+const bool useEmulator = true;
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-
   await Firebase.initializeApp();
+   if (useEmulator) {
+    await _connectToFirebaseEmulator();
+  }
   await CachHelper.init();
 
   Widget? widget;
@@ -31,4 +37,14 @@ void main() async {
   runApp(Xchat(
     startScreen: widget!,
   ));
+}
+
+Future _connectToFirebaseEmulator () async{
+  final localHostString = Platform.isAndroid?'10.0.2.2':'localhost';
+  FirebaseFirestore.instance.settings = Settings(
+    host: '$localHostString:8080',
+    sslEnabled: false,
+    persistenceEnabled:  false,
+  );
+  await FirebaseAuth.instance.useAuthEmulator(localHostString,9099);
 }
